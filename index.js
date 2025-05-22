e1ff7b5a93831ac57784274a38b252d9a9e8a721
Jake Bedford <robertjakebedford@gmail.com>
2025-05-22 16:34:42 -0600

    initial commit Unit2.Froyo

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log("Hello!");
+
+function getFroyoOrder() {
+  const input = prompt("Enter a list of froyo flavors (separated by commas):");
+
+  if (!input) {
+    console.log("No input provided. Please enter a list of flavors.");
+    return;
+  }
+}
+getFroyoOrder();
