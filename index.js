7a31ce4159fd919e075c6157b5c01b441fadbb81
Jake Bedford <robertjakebedford@gmail.com>
2025-05-22 17:16:20 -0600

    3rd commit Unit2.Froyo

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ function getFroyoOrder() {
     return;
   }
 }
-let (froyoFlavors = [Vanilla, Chocolate, Strawberry, Coffee]; i < froyoFlavors.length; i++) 
+let (froyoFlavors = [Vanilla,Chocolate,Strawberry,Coffee]) i < froyoFlavors.length; i++ {  
+} 
 getFroyoOrder();
 
-// This is where I am stuck. I need to figure out how to parse the input into an array of froyo flavors.
+// This is where I am stuck, more code below..... 
+// I need to figure out how to parse the input into an array of froyo flavors.
 
 // secondFlavor: Vanilla,
 // thirdFlavor: Vanilla,
@@ -20,7 +22,7 @@ getFroyoOrder();
 // sixthFlavor: Coffee
 
 
-//Build a website according to this user story:
+//Build a website according to this user story://
 
 //Upon opening the website, a visitor receives a prompt to enter a list of comma-separated froyo flavors.
 // They type vanilla,vanilla,vanilla,strawberry,coffee,coffee. In the browser console,
@@ -41,7 +43,18 @@ getFroyoOrder();
 // 4. Can you parse the user input into an array of froyo flavors?
 
 // 5. Can you build an object to track which flavors you have counted so far?
+let total= 0 
+for(froyoFlavor in froyo){
+  const flavorCount = froyo[froyoFlavor];
+  total += flavorCount;
+ }
+ return total;
+ console.log(totalFlavorCount);
+
 // 6. How should that object be updated as you iterate through the array of flavors?
+
 // 7. Are you using functions to organize your code?
+
 // 8. Do your variables and functions have meaningful names?
+
 // 9. Is your code formatted?
